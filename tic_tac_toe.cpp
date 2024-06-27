ff900e949bc1153428832d3550030bd801927400
nandinigithub22 <nandinijunghare07@gmail.com>
2024-06-27 22:20:37 +0530

    Create tic_tac_toe.cpp

---
+++ tic_tac_toe.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+using namespace std;
+
+char board[3][3];
+char currentPlayer = 'X';
+
+void initializeBoard() {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            board[i][j] = ' ';
+        }
+    }
+}
+
+void printBoard() {
+    cout << "  0 1 2\n";
+    for (int i = 0; i < 3; ++i) {
+        cout << i << " ";
+        for (int j = 0; j < 3; ++j) {
+            cout << board[i][j];
+            if (j < 2) cout << "|";
+        }
+        cout << "\n";
+        if (i < 2) cout << "  -+-+-\n";
+    }
+}
+
+bool isBoardFull() {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (board[i][j] == ' ') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool checkWin() {
+    // Check rows and columns
+    for (int i = 0; i < 3; ++i) {
+        if ((board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) ||
+            (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer)) {
+            return true;
+        }
+    }
+    // Check diagonals
+    if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+        (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
+        return true;
+    }
+    return false;
+}
+
+void switchPlayer() {
+    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+}
+
+void makeMove(int row, int col) {
+    if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+        board[row][col] = currentPlayer;
+    } else {
+        cout << "Invalid move. Try again.\n";
+    }
+}
+
+int main() {
+    int row, col;
+    initializeBoard();
+
+    while (true) {
+        printBoard();
+        cout << "Player " << currentPlayer << ", enter your move (row and column): ";
+        cin >> row >> col;
+
+        makeMove(row, col);
+
+        if (checkWin()) {
+            printBoard();
+            cout << "Player " << currentPlayer << " wins!\n";
+            break;
+        }
+
+        if (isBoardFull()) {
+            printBoard();
+            cout << "The game is a draw!\n";
+            break;
+        }
+
+        switchPlayer();
+    }
+
+    return 0;
+}
